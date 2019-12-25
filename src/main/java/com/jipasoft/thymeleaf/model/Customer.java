e6965c49f3d87a1d0fff9ef0398b50adbb40f42a
formationjav <sanogotech@gmail.com>
2019-12-25 19:59:59 +0000

    Update

---
+++ src/main/java/com/jipasoft/thymeleaf/model/Customer.java
@@ -0,0 +1,38 @@
+package com.jipasoft.thymeleaf.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer implements Serializable{
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String firstname;
+
+    @Column(nullable = false, length = 40)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "added_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
+    private Date addedDate;
+
+}
